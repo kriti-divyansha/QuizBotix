62541426329077243f75eebd7275bf8d4bf083d2
Kriti Divyansha <divyanshakriti@gmail.com>
2026-05-07 02:47:16 +0530

    Update fetch URL to use API_BASE_URL

--- frontend/src/pages/QuizPage.jsx
+++ frontend/src/pages/QuizPage.jsx
@@ -124,7 +124,7 @@ const QuizPage = () => {
         setError(null);
 
         try {
-            const res = await fetch('http://localhost:5000/submit-quiz', {
+            const res = await fetch(`${API_BASE_URL}/submit-quiz`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
